56eb1aa59aa4ec86756f108f170702e2e6fff469
Clint Baker <clintonjamesbaker@gmail.com>
2017-05-22 03:12:37 -0600

    Initial Commit, Add / Style Nav

---
+++ app/app.jsx
@@ -0,0 +1,18 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+const {Route, Router, IndexRoute, hashHistory} = require('react-router');
+const Main = require('Main');
+
+//Load foundation
+require('style-loader!css-loader!foundation-sites/dist/foundation.min.css');
+//App CSS
+require('style-loader!css-loader!sass-loader!applicationStyles')
+$(document).foundation();
+
+ReactDOM.render(
+  <Router history={hashHistory}>
+    <Route path="/" component={Main}>
+    </Route>
+  </Router>,
+  document.getElementById('app')
+);
---
+++ app/components/Main.jsx
@@ -0,0 +1,17 @@
+const React = require('react');
+var Nav = require('Nav');
+
+var Main = (props) => {
+  return (
+    <div>
+      <Nav/>
+      <div>
+        <div>
+          {props.children}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+module.exports = Main;
